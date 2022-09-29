b5914dfc82062f883796e546e2a3f4d7afd15718
erik <morbacherik@gmail.com>
2022-09-29 15:55:51 -0300

    Só abre a pinça se estiver em Idle

--- bCNC/__main__.py
+++ bCNC/__main__.py
@@ -472,7 +472,7 @@ class Application(Toplevel, Sender):
         global clampToggleCounter
         clampToggleCounter = 0
         def clampToggle(*args):
-            if self.serial is None: return
+            if self.serial is None or CNC.vars['state'].lower() not in ["idle"]: return
             global clampToggleCounter
             if clampToggleCounter % 2 == 0: self.serial_write(chr(0xA5))
             else: self.serial_write(chr(0xA6))
