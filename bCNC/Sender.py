353d2bdaac44ad7c68f655adb4ad1472d6669391
erik <morbacherik@gmail.com>
2022-10-20 12:01:54 -0300

    Teste de Referência

--- bCNC/Sender.py
+++ bCNC/Sender.py
@@ -242,6 +242,7 @@ class Sender:
 		while "home" in CNC.vars["state"].lower():
 			time.sleep(0.1)
 		self.sendGCode("G0X-20")
+		self.sendGCode("G0X-5")
 		time.sleep(1)
 		while "run" in CNC.vars["state"].lower():
 			time.sleep(0.1)
