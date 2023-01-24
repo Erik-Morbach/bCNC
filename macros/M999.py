a35fc494180f9f368b86d18534c5889c4ec01057
Erik <morbacherik@gmail.com>
2023-01-24 17:29:20 -0300

    Setando corretamente todas as coordenadas para -4

--- macros/M999.py
+++ macros/M999.py
@@ -1,2 +1,8 @@
-for i in range(1, 13):
-    set("Motor{}Position".format(i), -4)
+code("M123")
+code("M456")
+for i in range(7, 13):
+    code("#Motor{}Position".format(i), -4)
+code("M789")
+code("M101112")
+for i in range(1, 7):
+    code("#Motor{}Position".format(i), -4)
