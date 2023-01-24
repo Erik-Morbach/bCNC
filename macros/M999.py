0f1bd55002e508c2abde1281888793c4cf17be3d
Erik <morbacherik@gmail.com>
2023-01-24 17:33:00 -0300

    Erro ao mudar o metodo utilizado para setar os valores

--- macros/M999.py
+++ macros/M999.py
@@ -1,8 +1,8 @@
 code("M123")
 code("M456")
 for i in range(7, 13):
-    code("#Motor{}Position".format(i), -4)
+    code("#Motor{}Position = -4".format(i))
 code("M789")
 code("M101112")
 for i in range(1, 7):
-    code("#Motor{}Position".format(i), -4)
+    code("#Motor{}Position = -4".format(i))
