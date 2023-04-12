38f8ef7071a95675c2f2e8a48ac9c5975986a139
Erik <morbacherik@gmail.com>
2023-04-12 16:21:19 -0300

    Clamp script usando CNC.vars

--- scripts/ClampToggle.py
+++ scripts/ClampToggle.py
@@ -1,12 +1,12 @@
-try:
-    global clampToggleCounter
-    clampToggleCounter
-except:
-    clampToggleCounter = 0
+clampToggleName = "ClampToggleCounter"
+if clampToggleName not in CNC.vars.keys():
+    CNC.vars[clampToggleName] = 0
+print(CNC.vars[clampToggleName])
 if self.serial is None or CNC.vars['state'].lower() not in ["idle"]: 
     pass
 else:
-    if clampToggleCounter % 2 == 0: self.serial_write(chr(0xA5))
+    if CNC.vars[clampToggleName] % 2 == 0: self.serial_write(chr(0xA5))
     else: self.serial_write(chr(0xA6))
     self.serial.flush()
-    clampToggleCounter+=1
+    CNC.vars[clampToggleName] += 1
+    
