6977761e723f36245266d7df4159b4e0d2ed8b7c
Erik Morbach <morbacherik@gmail.com>
2024-03-27 16:12:47 -0300

    View Usando gpiozero

--- view.py
+++ view.py
@@ -1,7 +1,6 @@
-import wiringpi
 import os
+import gpiozero
 
-wiringpi.wiringPiSetupGpio()
 
 pins = []
 
@@ -9,15 +8,18 @@ pins += [[20,21]]
 pins += [[13,16,19,26]]
 pins += [[5,12,6]]
 pins += [[7,9,11,8]]
+obj = []
 for block in pins:
+    obj += [[]]
+
     for pin in block:
-        wiringpi.pinMode(pin, 0)
+        obj[-1] += [gpiozero.Button(pin, pull_up=False)]
 
 while 1:
     os.system("clear")
     st = ""
-    for block in pins:
+    for (pins, ob) in zip(pins, obj):
         st += "-"*20 + "\n"
-        st += " ".join([str(w) for w in block]) + "\n"
-        st += " ".join([str(wiringpi.digitalRead(w)) for w in block]) + "\n"
+        st += " ".join([str(w) for w in pins]) + "\n"
+        st += " ".join([str(w.is_pressed) for w in ob]) + "\n"
     print(st, end='')
